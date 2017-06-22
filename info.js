893a8952a21d03d8aa77aec20714343edd0981df
Joe Dai <dm.joedai@gmail.com>
2017-06-22 12:07:28 -0700

    Add version and copyright info

--- info.js
+++ info.js
@@ -7,4 +7,5 @@ var nprogress_version = 'v' + NProgress.version;
 var simpleweather_version = 'v3.1.0';
 var mousetrap_version = 'v1.4.6';
 
+document.getElementById("version").innerHTML = version;
 console.log("JD.start version: " + version);
